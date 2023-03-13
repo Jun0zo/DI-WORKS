f9134aa5ab3ce9eb69c17dc2f74e1176f5dcd180
Jun0zo <joon0zo1022@gmail.com>
2023-03-14 08:18:49 +0900

    Layout구조 확립

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../components/layout/Layout";
+function Home() {
+  return (
+    <div>
+      <Layout>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of our website.</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default Home;
