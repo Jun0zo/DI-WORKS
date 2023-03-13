d8bda6136829d4c9a6a131ef45740aa8b68cda92
Jun0zo <joon0zo1022@gmail.com>
2023-03-14 02:27:40 +0900

    라우터 기능 추가 (초기 세팅)

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        You can contact us at example@example.com or by phone at (123) 456-7890.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
